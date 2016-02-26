eec20aa04e7ce5d49f557a4dd06af9b6573d8879
sashikiran9 <devop.sashi@gmail.com>
2016-02-26 00:51:26 -0500

    jh
    jhg

--- Ch6/src/main/hql/monthly_violence_totals.sql
+++
@@ -1,5 +0,0 @@
-
-SELECT from_unixtime(unix_timestamp(event_date, 'yyyy-MM-dd'),'yyyy-MMM'),
-       COALESCE(CAST(sum(fatalities) as STRING), 'Unknown')
-       FROM acled_nigeria_cleaned
-       GROUP BY from_unixtime(unix_timestamp(event_date, 'yyyy-MM-dd'),'yyyy-MMM');
